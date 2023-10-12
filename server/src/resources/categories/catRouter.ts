b9ce2bd06f60771892492f0b262d4be2078764c3
heinanX <larrii@live.se>
2023-10-12 14:00:27 +0200

    validation categories

--- server/src/resources/categories/catRouter.ts
+++ server/src/resources/categories/catRouter.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
 import { createCategory } from './catController';
 import { formatData } from '../../_middlewares/catMiddleware';
+import { catJoiSchema } from './catModel';
+import { validate } from '../../_middlewares/shared';
+
 export const catRouter = Router();
 
-catRouter.post('/', formatData, createCategory);
+catRouter.post('/', validate(catJoiSchema), formatData, createCategory);
