fd97b6f4f52411a16c36d359b1a1acccbbb12a62
heinanX <larrii@live.se>
2023-10-16 13:08:54 +0200

    Comment: GET comment

--- server/src/resources/comments/commentsController.ts
+++ server/src/resources/comments/commentsController.ts
@@ -1,11 +1,36 @@
 import { NextFunction, Request, Response } from "express";
 import { CommentsModel } from "./commentsModel";
 
-export const createComment = async( req: Request, res: Response, next: NextFunction) => {
-    try {
-        const comment = await CommentsModel.create(req.body)
-        res.status(200).json(comment);
-    } catch (error) {
-        res.status(404).json(error);
-    }
-}
+// GET ALL COMMENTS FROM DB
+export const getComments = async (req: Request, res: Response) => {
+  try {
+    const comments = await CommentsModel.find();
+    res.status(200).json(comments);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+};
+
+//GET ONE COMMENT FROM DB
+export const getComment = async (req: Request, res: Response) => {
+  try {
+    const comment = await CommentsModel.findOne({ _id: req.params.id });
+    res.status(200).json(comment);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+};
+
+// CREATE A COMMENT IN DATABASE
+export const createComment = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const comment = await CommentsModel.create(req.body);
+    res.status(200).json(comment);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+};
